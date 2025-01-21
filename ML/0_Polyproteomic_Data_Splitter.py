e3c578638af8a7d845172a3bbfcd3e9fb8caba24
jon126 <jonathan.chan0@protonmail.com>
2025-01-21 18:51:03 +1300

    Init scripts to perform train_test splitting separately

---
+++ ML/0_Polyproteomic_Data_Splitter.py
@@ -0,0 +1,66 @@
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+import argparse
+import os
+
+parser = argparse.ArgumentParser(description='Split a dataset using 80/20 split.')
+parser.add_argument('--data_path', type=str, default='', help="The model data containing both X and Y")
+parser.add_argument('--data_output_folder', type=str, required=True, help="Folder path to save the plots and model")
+parser.add_argument('--target_variable', type=str, default='', help="The target variable is either prevalent (i.e HCM case/control status) or incident (i.e incident HCM diagnosis)")
+args = parser.parse_args()
+
+# Create the output folder if it does not exist
+if not os.path.exists(args.plot_output_folder):
+    os.makedirs(args.plot_output_folder)
+
+print("Importing data...")
+
+# Import data
+exclusion_list = pd.read_csv('../DATA/UKB/w11223_20241217.csv', header=None)
+exclusion_list.columns=['eid'] #Rename the column to 'eid'
+pp_i0_covariates = pd.read_csv(args.data_path, sep='\t') #Already filtered for individuals which are at least non-NA in one Olink pp and non-NA values in covariates.
+
+print(exclusion_list.shape)
+print(pp_i0_covariates.shape)
+
+#Filter the pp and the covariate dataframes for individuals not in exclusion list
+pp_i0_covariates = pp_i0_covariates[~pp_i0_covariates['eid'].isin(exclusion_list['eid'])]
+print(pp_i0_covariates.shape) 
+
+#Print the number of HCM cases and controls in pp_i0_covariates after exclusion
+print(pp_i0_covariates['hcm'].value_counts())
+
+#Extract out the X and y variables from the pp_i0_covariates dataframe where the y variable is labelled 'hcm' column and the x variable is all other columns
+if args.target_variable == 'prevalent':
+    X = pp_i0_covariates.drop(columns=['eid', 'hcm', 'instance'])
+    y = pp_i0_covariates['hcm']
+elif args.target_variable == 'incident':
+    X = pp_i0_covariates.drop(columns=['eid', 'incidenthcm_status', 'instance'])
+    y = pp_i0_covariates['incidenthcm_status']
+
+print("Data import completed.")
+
+print("Splitting data into training and test sets...")
+
+#Split off the X and y dataframes into a 80% training and 20% test set
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, stratify=y, random_state=42)
+
+print(X_train.shape)
+print(X_test.shape)
+
+#Print the number of HCM=true and HCM=false in the training and test set
+print(y_train.value_counts())
+print(y_test.value_counts())
+
+print("Data splitting completed.")
+
+print("Saving training and test sets to CSV files...")
+
+#Write out the training and test sets to csv files
+X_train.to_csv(os.path.join(args.data_output_folder,'X_train.csv'), index=False)
+X_test.to_csv(os.path.join(args.data_output_folder,'X_test.csv'), index=False)
+y_train.to_csv(os.path.join(args.data_output_folder,'y_train.csv'), index=False)
+y_test.to_csv(os.path.join(args.data__output_folder,'y_test.csv'), index=False)
+
+print("CSV files saved.")
---
+++ ML/0_Polyproteomic_Data_Splitter_sbatch.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#SBATCH -p short
+#SBATCH -A procardis.prj
+#SBATCH --constraint="skl-compat"
+#SBATCH -c 2
+
+module load Miniforge3/24.1.2-0
+eval "$(conda shell.bash hook)"
+conda activate python3.11_ml
+
+# Set the number of threads for Python
+export OMP_NUM_THREADS=2
+
+#Scenario 1: Case control status prediction (5Y)
+# python 0_Polyproteomic_Data_Splitter.py \
+#     --data_path '../DATA/UKB/PROCESSED/2_5Y/ukb_ALLpanel_pp_hcm_covariates_bp_t2d_smoking_rcmmcov.tsv' \
+#     --data_output_folder '../OUTPUT/UKB/ML/1_data/1_hcm_cc_noprs/' \
+#     --target_variable 'prevalent' 
+
+# Scenario 2: Incident HCM prediction from the 5Y controls
+python 0_Polyproteomic_Data_Splitter.py \
+    --data_path '../DATA/UKB/PROCESSED/2_5Y/ukb_ALLpanel_pp_hcm_covariates_bp_t2d_smoking_rcmmcov_CoxHCMDiag.tsv' \
+    --data_output_folder '../OUTPUT/UKB/ML/1_data/3_hcm_incident_no_prs/' \
+    --target_variable 'incident' 
+
+#N.B You have to append the cv_prs column manually to the X_train, X_test, y_train, y_test columns
