f99cfaa8aaf5fbd6d8cb31238b28fcf816fd435d
jon126 <jonathan.chan0@protonmail.com>
2025-03-03 19:13:12 +0800

    Fix feature importance plotting

--- ML/2_Polyproteomic_FeatureImportance_Evaluator.py
+++ ML/2_Polyproteomic_FeatureImportance_Evaluator.py
@@ -19,105 +19,97 @@ def load_model(model_path):
     print(f"Loading model from {model_path}")
     return joblib.load(model_path)
 
-def plot_feature_importance(model, X, feature_names, model_name, output_folder, features_filtered=None, n_features_to_plot=30):
-        """Plot feature importance for the given model."""
-        print(f"Plotting feature importance for {model_name}")
+def plot_feature_importance(model, feature_names, model_name, output_folder, features_filtered=None, n_features_to_plot=30):
+    """Plot feature importance for the given model."""
+    print(f"Plotting feature importance for {model_name}")
 
-        num_features = len(feature_names)
+    num_features = len(feature_names)
 
-        if num_features < n_features_to_plot * 2:  # If the actual number of features after selection is close but > n_features_to_plot
-            n_features_to_plot = num_features + 1
+    if num_features < n_features_to_plot * 2:  # If the actual number of features after selection is close but > n_features_to_plot
+        n_features_to_plot = num_features + 1
 
-        if hasattr(model, 'feature_importances_') or (hasattr(model, 'named_steps') and 'classifier' in model.named_steps and hasattr(model.named_steps['classifier'], 'feature_importances_')):
-            # Tree-based models (Random Forest, Gradient Boosting)
-            if hasattr(model, 'feature_importances_'):
-                importances = model.feature_importances_
-            else:
-                importances = model.named_steps['classifier'].feature_importances_
-
-            indices = np.argsort(importances)[::-1]
-            sorted_features = feature_names[indices][:n_features_to_plot]
-            sorted_importances = importances[indices][:n_features_to_plot]
-            plot_suffix=''
-
-            if features_filtered is not None:
-                # Filter the features
-                mask = np.isin(sorted_features, features_filtered)
-                sorted_features = sorted_features[mask]
-                sorted_importances = sorted_importances[mask]
-                plot_suffix = '_filtered'
-
-            plt.figure(figsize=(12, 9))
-            sns.barplot(x=sorted_importances, y=sorted_features)
-            plt.title(f'Feature Importance - {model_name}')
-            plt.xlabel('Importance')
-            plt.ylabel('Features')
-            plt.tight_layout()
-            plt.savefig(os.path.join(output_folder, f'{model_name}_feature_importances{plot_suffix}.png'))
-            plt.close()
-            print(f"Feature importance plot saved for {model_name}")
-
-        elif hasattr(model, 'coef_') or (hasattr(model, 'named_steps') and 'classifier' in model.named_steps and hasattr(model.named_steps['classifier'], 'coef_')):
-            # Linear models (Logistic Regression)
-            if hasattr(model, 'coef_'):
-                importances = model.coef_[0]
-            else:
-                importances = model.named_steps['classifier'].coef_[0]
-            indices = np.argsort(np.abs(importances))[::-1]
-            sorted_features = feature_names[indices][:n_features_to_plot]
-            sorted_importances = importances[indices][:n_features_to_plot]
-            plot_suffix=''
-
-            if features_filtered is not None:
-                # Filter the features
-                mask = np.isin(sorted_features, features_filtered)
-                sorted_features = sorted_features[mask]
-                sorted_importances = sorted_importances[mask]
-                plot_suffix = '_filtered'
-
-            plt.figure(figsize=(12, 9))
-            sns.barplot(x=sorted_importances, y=sorted_features)
-            plt.title(f'Feature Coefficients - {model_name}')
-            plt.xlabel('Coefficient')
-            plt.ylabel('Features')
-            plt.tight_layout()
-            plt.savefig(os.path.join(output_folder, f'{model_name}_feature_importances{plot_suffix}.png'))
-            plt.close()
-            print(f"Feature coefficients plot saved for {model_name}")
+    if hasattr(model, 'feature_importances_') or (hasattr(model, 'named_steps') and 'classifier' in model.named_steps and hasattr(model.named_steps['classifier'], 'feature_importances_')):
+        plot_label = 'Importances'
+        if hasattr(model, 'feature_importances_'):
+            importances = model.feature_importances_
         else:
-            # Use SHAP values for other models
-            print(f"Calculating SHAP values for {model_name}")
-            if hasattr(model, 'named_steps') and 'classifier' in model.named_steps:
-                classifier = model.named_steps['classifier']
-            else:
-                classifier = model
-
-            if 'eid' in X.columns:
-                X = X.drop(columns='eid')
-
-            background = shap.sample(X, 100, random_state=42)  # Use a sample of the data as the background
-            explainer = shap.KernelExplainer(classifier.predict, background)
-            shap_values = explainer.shap_values(X)
-            plot_suffix = ''
-
-            if features_filtered is not None:
-                # Filter the features
-                mask = np.isin(feature_names, features_filtered)
-                feature_names = feature_names[mask]
-                shap_values = shap_values[mask]
-                plot_suffix='_filtered'
+            importances = model.named_steps['classifier'].feature_importances_
+    elif hasattr(model, 'coef_') or (hasattr(model, 'named_steps') and 'classifier' in model.named_steps and hasattr(model.named_steps['classifier'], 'coef_')):
+        plot_label = 'Coefficients'
+        
+        if hasattr(model, 'coef_'):
+            importances = model.coef_[0]
+        else:
+            importances = model.named_steps['classifier'].coef_[0]
             
-            shap.summary_plot(shap_values, X, feature_names=feature_names, max_display=n_features_to_plot, show=False)
-            plt.title(f'SHAP Feature Importance - {model_name}')
-            plt.tight_layout()
-            plt.savefig(os.path.join(output_folder, f'{model_name}_feature_importances{plot_suffix}.png'))
-            plt.close()
-            print(f"SHAP feature importance plot saved for {model_name}")
+    if features_filtered is not None:
+        # Filter the features if features_filtered is defined
+        mask = np.isin(feature_names, features_filtered)
+        feature_names = feature_names[mask]
+        importances = importances[mask]
+        plot_suffix = '_featurefiltered'
+    else:
+        plot_suffix=''
 
-            # Save SHAP values
-            shap_values_path = os.path.join(output_folder, f'{model_name}_shap_values{plot_suffix}.npy')
-            np.save(shap_values_path, shap_values)
-            print(f"SHAP values saved for {model_name} at {shap_values_path}")
+    #Sort the features by importance + filter to the n_features_to_plot
+    indices = np.argsort(importances)[::-1]
+    sorted_features = feature_names[indices][:n_features_to_plot]
+    sorted_importances = importances[indices][:n_features_to_plot]
+
+    plt.figure(figsize=(12, 9))
+    sns.barplot(x=sorted_importances, y=sorted_features)
+    plt.title(f'Feature {plot_label} - {model_name}')
+    plt.xlabel(f'{plot_label}')
+    plt.ylabel('Features')
+    plt.tight_layout()
+    plt.savefig(os.path.join(output_folder, f'{model_name}_feature_importances{plot_suffix}.png'))
+    plt.close()
+    print(f"Feature coefficients plot saved for {model_name}")
+
+    #Output the feature importances as a csv file with the respective feature names as well
+    feature_importances_df = pd.DataFrame({'Feature': sorted_features, 'Importance': sorted_importances})
+    feature_importances_df.to_csv(os.path.join(output_folder, f'{model_name}_feature_importances{plot_suffix}.csv'), index=False)
+    print(f"Feature importances saved for {model_name}")
+
+    return feature_importances_df
+
+def plot_Fratio(fratio_df, model_name,output_folder, features_filtered=None):
+    """Plot the F-ratio for the given model."""
+    print(f"Plotting F-ratio for {model_name}")
+
+    fratio_df = fratio_df.sort_values('F-ratio', ascending=False)
+
+    if features_filtered is not None:
+        mask =np.isin(features_filtered,fratio_df['Feature'])
+        features_of_interest=features_filtered[mask]
+        fratio_df = fratio_df[fratio_df['Feature'].isin(features_of_interest)]
+
+    plt.figure(figsize=(12, 9))
+    sns.barplot(x='F-ratio', y='Feature', data=fratio_df)
+    plt.title(f'F-ratio - {model_name}')
+    plt.xlabel('F-ratio')
+    plt.ylabel('Features')
+    plt.tight_layout()
+    plt.savefig(os.path.join(output_folder, f'{model_name}_fratio.png'))
+    plt.close()
+    print(f"F-ratio plot saved for {model_name}")
+
+def plot_feature_importance_vs_Fratio(feature_importances_df, fratio_df, model_name, output_folder):
+    """Plot feature importance vs F-ratio for the given model."""
+    print(f"Plotting feature importance vs F-ratio for {model_name}")
+
+    # Merge the feature importances and F-ratio dataframes
+    merged_df = feature_importances_df.merge(fratio_df, on='Feature', how='inner')
+
+    plt.figure(figsize=(12, 9))
+    sns.scatterplot(x='Importance', y='F-ratio', data=merged_df)
+    plt.title(f'Feature Importance vs F-ratio - {model_name}')
+    plt.xlabel('Importance')
+    plt.ylabel('F-ratio')
+    plt.tight_layout()
+    plt.savefig(os.path.join(output_folder, f'{model_name}_feature_importance_vs_fratio.png'))
+    plt.close()
+    print(f"Feature importance vs F-ratio plot saved for {model_name}")
 
 if __name__ == "__main__":
     # Parse command line arguments
@@ -126,6 +118,7 @@ if __name__ == "__main__":
     parser.add_argument('--model_pkl_file', type=str, required=True, help="Folder path containing the saved models")
     parser.add_argument('--output_folder', type=str, required=True, help="Folder path to save the feature importance plots")
     parser.add_argument('--X_data_file', type=str, required=True, help="CSV file containing the preprocessed data used for training")
+    parser.add_argument('--y_data_file', type=str, required=True, help="CSV file containing the target labels")
     parser.add_argument('--pp_names_file', type=str, required=True, help="CSV file containing the plasma protein names")
     args = parser.parse_args()
 
@@ -149,11 +142,19 @@ if __name__ == "__main__":
     feature_names = preprocessor2.get_feature_names_out()
     
     #This plots the feature importance for all the features i.e both clinical covariates and plasma proteins
-    plot_feature_importance(model, X, feature_names, args.model_name, args.output_folder)
+    plot_feature_importance(model,feature_names, args.model_name, args.output_folder)
 
     #Load the plasma protein feature names
     pp_names = pd.read_csv(args.pp_names_file)['name'].values
+
     #Plot feature importances for only the plasma proteins
-    plot_feature_importance(model, X, feature_names, args.model_name, args.output_folder, features_filtered=pp_names)
+    feature_importances_df = plot_feature_importance(model, feature_names, args.model_name, args.output_folder, features_filtered=pp_names)
+
+    #Compute the F-ratio for the plasma proteins in the X_train data file given that X is a dataframe
+    fvalues = sklearn.feature_selection.f_classif(X.loc[:,pp_names], pd.read_csv(args.y_data_file).values.ravel())[0]
+    fratio_df = pd.DataFrame({'Feature': pp_names, 'F-ratio': fvalues})
+
+    plot_Fratio(fratio_df, args.model_name, args.output_folder, features_filtered=pp_names)
+    plot_feature_importance_vs_Fratio(feature_importances_df, fratio_df, args.model_name, args.output_folder)
 
     print(f"Completed feature importance evaluation for {args.model_name}")
--- ML/archive/2_Polyproteomic_FeatureImportance_sbatch.sh
+++ ML/archive/2_Polyproteomic_FeatureImportance_sbatch.sh
@@ -13,17 +13,19 @@ conda activate python3.11_ml
 # Set the number of threads for Python
 export OMP_NUM_THREADS=4
 
-echo 'Evaluating feature importance'
-python 2_Polyproteomic_FeatureImportance_Evaluator.py \
-    --model_name xgboost \
-    --model_pkl_file ../OUTPUT/UKB/ML/2_models/1_hcm_cc_noprs/xgboost_best_model.pkl \
-    --output_folder ../OUTPUT/UKB/ML/3_summary_plots/feature_importance/1_hcm_cc_noprs/ \
-    --X_data_file ../OUTPUT/UKB/ML/1_data/1_hcm_cc_noprs/X_train_preprocessed_xgboost.csv \
-    --pp_names_file ../DATA/UKB/ML/2_covariates_pp/ML_pp_names.csv
+model_name=xgboost #Definet the model name here e.g logistic_regression, xgboost, svm, l1_logistic_regression_nofs
 
+model_pkl_file=../OUTPUT/UKB/ML/2_models/1_hcm_cc_noprs/"$model_name"_best_model.pkl
+output_folder=../OUTPUT/UKB/ML/3_summary_plots/feature_importance/1_hcm_cc_noprs/
+X_data_file=../OUTPUT/UKB/ML/1_data/1_hcm_cc_noprs/X_train_preprocessed_"$model_name".csv
+y_data_file=../OUTPUT/UKB/ML/1_data/1_hcm_cc_noprs/y_train.csv
+pp_names_file=../DATA/UKB/ML/2_covariates_pp/ML_pp_names.csv
+
+echo 'Evaluating feature importance for' $model_name
 python 2_Polyproteomic_FeatureImportance_Evaluator.py \
-    --model_name xgboost \
-    --model_pkl_file ../OUTPUT/UKB/ML/2_models/5_hcm_allcases_noprs/xgboost_best_model.pkl \
-    --output_folder ../OUTPUT/UKB/ML/3_summary_plots/feature_importance/5_hcm_allcases_noprs/ \
-    --X_data_file ../OUTPUT/UKB/ML/1_data/5_hcm_allcases_noprs/X_train_preprocessed_xgboost.csv \
-    --pp_names_file ../DATA/UKB/ML/2_covariates_pp/ML_pp_names.csv
+    --model_name $model_name \
+    --model_pkl_file $model_pkl_file \
+    --output_folder $output_folder \
+    --X_data_file $X_data_file \
+    --y_data_file $y_data_file \
+    --pp_names_file $pp_names_file
