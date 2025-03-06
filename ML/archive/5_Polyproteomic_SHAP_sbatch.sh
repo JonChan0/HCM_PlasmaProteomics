54fce4197a5c949f6be0e4d390bf3cf36e435776
jon126 <jonathan.chan0@protonmail.com>
2025-03-06 17:49:55 +0800

    Minor

--- ML/archive/5_Polyproteomic_SHAP_sbatch.sh
+++ ML/archive/5_Polyproteomic_SHAP_sbatch.sh
@@ -12,12 +12,12 @@ conda activate python3.11_ml
 # Set the number of threads for Python
 export OMP_NUM_THREADS=2
 
-model_name=xgboost #Definet the model name here e.g logistic_regression, xgboost, svm, l1_logistic_regression_nofs
+model_name=xgboost_nofs #Definet the model name here e.g logistic_regression, xgboost, svm, l1_logistic_regression_nofs
 echo Performing SHAP computation for trained model $model_name
 
 python 5_Polyproteomic_SHAP.py \
     --model_pkl_file ../OUTPUT/UKB/ML/2_models/1_hcm_cc_noprs/"$model_name"_best_model.pkl \
-    --model_name xgboost \
+    --model_name $model_name \
     --X_train_preprocessed_path ../OUTPUT/UKB/ML/1_data/1_hcm_cc_noprs/X_train_preprocessed_"$model_name".csv \
     --y_train_path ../../OUTPUT/UKB/ML/1_data/1_hcm_cc_noprs/y_train.csv \
     --plot_output_path ../OUTPUT/UKB/ML/3_summary_plots/feature_importance/1_hcm_cc_noprs/shap \
