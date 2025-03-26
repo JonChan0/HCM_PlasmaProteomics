43de9ec0a0c4d252680165ab08dc304d54bb8803
jon126 <jonathan.chan0@protonmail.com>
2025-03-26 15:40:48 -0500

    Minor

--- ML/archive/5_Polyproteomic_SHAP_sbatch.sh
+++ ML/archive/5_Polyproteomic_SHAP_sbatch.sh
@@ -19,6 +19,6 @@ python 5_Polyproteomic_SHAP.py \
     --model_pkl_file ../OUTPUT/UKB/ML/2_models/1_hcm_cc_noprs/"$model_name"_best_model.pkl \
     --model_name $model_name \
     --X_train_preprocessed_path ../OUTPUT/UKB/ML/1_data/1_hcm_cc_noprs/X_train_preprocessed_"$model_name".csv \
-    --y_train_path ../../OUTPUT/UKB/ML/1_data/1_hcm_cc_noprs/y_train.csv \
+    --y_train_path ../OUTPUT/UKB/ML/1_data/1_hcm_cc_noprs/y_train.csv \
     --plot_output_path ../OUTPUT/UKB/ML/3_summary_plots/feature_importance/1_hcm_cc_noprs/shap \
     --pp_names_file=../DATA/UKB/ML/2_covariates_pp/ML_pp_names.csv
