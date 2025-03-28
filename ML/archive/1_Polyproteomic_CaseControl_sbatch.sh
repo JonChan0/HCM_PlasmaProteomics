269f991b54bb158264b10c72b023a809392a023d
jon126 <jonathan.chan0@protonmail.com>
2025-03-28 10:26:01 -0500

    Add submission for xgboost model with only covariates + 9 FDR-passing plasma proteins from case-control analysis

--- ML/archive/1_Polyproteomic_CaseControl_sbatch.sh
+++ ML/archive/1_Polyproteomic_CaseControl_sbatch.sh
@@ -57,6 +57,15 @@ y_train_data_path='../OUTPUT/UKB/ML/1_data/1_hcm_cc_noprs/y_train.csv'
 #     --features_to_bypass_fs $features_to_bypass --features_to_select_fs $features_to_select  \
 #     --target_variable $target_variable --X_train_data $X_train_data_path --y_train_data $y_train_data_path
 
+# echo 'Training model for xgboost with ONLY covariates + 9 FDR-passing plasma proteins from case-control analysis'
+# model_output_folder='../OUTPUT/UKB/ML/2_models/1_hcm_cc_noprs/covariates_cc_top9/'
+# features_to_select='../DATA/UKB/ML/2_covariates_pp/cc_top9_names.csv'
+# python 1_Polyproteomic_ML_Train.py --model_name xgboost --plot_output_folder "$model_output_folder"4_XGB/ \
+#     --feature_selection False \
+#     --model_output_folder $model_output_folder\
+#     --features_to_bypass_fs $features_to_bypass --features_to_select_fs $features_to_select  \
+#     --target_variable $target_variable --X_train_data $X_train_data_path --y_train_data $y_train_data_path
+
 # echo 'Training model for svm'
 # python 1_Polyproteomic_ML_Train.py --model_name svm --plot_output_folder "$model_output_folder"5_SVM \
 #     --feature_selection True \
